710f94cb7a6f20197b16452ab9b6c8bb9de17e14
Oliver <oliverm.development@proton.me>
2025-08-31 22:44:27 -0700

    fix: small redability changes

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -9,23 +9,25 @@ class ModerationLogger():
 
     async def log_ban(self, ctx: ApplicationContext, member: User, reason, evidence, notes):
         embed = load_embed_from_yaml("banlog.yaml")
-        embed.fields = [field for field in embed.fields 
-                if not ((field.name == "Evidence" and evidence is None) or
-                        (field.name == "Ban Note" and notes is None))]
-
+        embed.fields = [field for field in embed.fields if not ((field.name == "Evidence" and evidence is None) or (field.name == "Ban Note" and notes is None))]
         embed.thumbnail = member.display_avatar.url
-        embed = load_data_into_embed(embed,
-                                     {"user_id": member.id, "moderator_id": ctx.author.id, "reason": reason, "evidence": evidence, "note": notes, "timestamp_unix": str(int(time.time()))}
-                                     )
+        embed = load_data_into_embed(embed, 
+                                     {"user_id": member.id, 
+                                      "moderator_id": ctx.author.id, 
+                                      "reason": reason, 
+                                      "evidence": evidence, 
+                                      "note": notes, 
+                                      "timestamp_unix": str(int(time.time()))
+                                      })
         await log_info_to_guild(ctx, "moderation", embed)
 
     async def log_unban(self, ctx: ApplicationContext, member: User, notes):
         embed = load_embed_from_yaml("unbanlog.yaml")
-        embed.fields = [field for field in embed.fields 
-                if not (field.name == "Note" and notes is None)]
-
+        embed.fields = [field for field in embed.fields if not (field.name == "Note" and notes is None)]
         embed.thumbnail = member.display_avatar.url
         embed = load_data_into_embed(embed,
-                                     {"user_id": member.id, "moderator_id": ctx.author.id, "note": notes, "timestamp_unix": str(int(time.time()))}
-                                     )
+                                     {"user_id": member.id, 
+                                      "moderator_id": ctx.author.id, 
+                                      "note": notes, 
+                                      "timestamp_unix": str(int(time.time()))})
         await log_info_to_guild(ctx, "moderation", embed)
