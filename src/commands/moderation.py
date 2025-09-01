35c1834bfb9c9acc47256d3f22a3ec5592a379e2
Oliver <oliverm.development@proton.me>
2025-08-31 22:43:36 -0700

    fix: redability changes, & easier updating of emojis

--- src/commands/moderation.py
+++ src/commands/moderation.py
@@ -1,23 +1,28 @@
 import discord
 from discord.ext import commands
+from os import getenv
+from asyncio import create_task
 from auth.permissions import check_permissions_for_command
 from utils.autocompleter import banned_users_autocomplete
 from utils.embeds import load_embed_from_yaml, load_data_into_embed
 from utils.logger import ModerationLogger
 
 logger = ModerationLogger()
+ban_emoji = getenv("BAN_EMOJI")
+unban_emoji = getenv("UNBAN_EMOJI")
+failed_emoji = getenv("LOGIC_FAIL_EMOJI")
 
 class Moderation(commands.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
 
     @discord.slash_command(
-            name="purge", 
-            description="Delete a number of messages from this channel",
-            default_member_permissions=discord.Permissions(
-                manage_messages=True
-                )
-            )
+        name="purge", 
+        description="Delete a number of messages from this channel",
+        default_member_permissions=discord.Permissions(
+            manage_messages=True
+        )
+    )
     @commands.check(check_permissions_for_command)
     @commands.bot_has_permissions(manage_messages=True)
     async def purge(self, ctx: discord.ApplicationContext, amount: int):
@@ -25,8 +30,8 @@ class Moderation(commands.Cog):
             await ctx.response.send_message("You can purge between 1 and 100 messages.", ephemeral=True)
             return
 
-        deleted = await ctx.channel.purge(limit=amount)
-        await ctx.response.send_message(f"Bulk deleted {len(deleted)} messages.", ephemeral=True)
+        deleted_messages = await ctx.channel.purge(limit=amount)
+        await ctx.response.send_message(f"Bulk deleted {len(deleted_messages)} messages.", ephemeral=True)
 
     @discord.slash_command(
         name="ban",
@@ -41,35 +46,25 @@ class Moderation(commands.Cog):
         self, 
         ctx: discord.ApplicationContext, 
         member: discord.Member, 
-        reason: str = None,
+        reason: str = "No Reason Provided",
         evidence: str = None,
         notes: str = None,
     ):
-        reason = reason or "No Reason Provided"
-        try:
-            ban_embed = load_data_into_embed(load_embed_from_yaml("recievedban.yaml"), 
-                                             {"server": ctx.guild.name, 
-                                              "moderator_id": str(ctx.author.id),
-                                              "reason": reason,
-                                              }
-                                             )
+        ban_embed_yaml = load_embed_from_yaml("recievedban.yaml")
+        ban_embed = load_data_into_embed(ban_embed_yaml, {"server": ctx.guild.name, "moderator_id": str(ctx.author.id),"reason": reason,})
 
-            await member.send(embed=ban_embed)
-        except discord.Forbidden:
-            pass
+        create_task(member.send(embed=ban_embed))
 
         await ctx.guild.ban(member, reason=reason)
         await logger.log_ban(ctx, member, reason, evidence, notes)
         await ctx.response.send_message(
-            f"<:ban_hammer:1411243266246049883> {member.mention} has been banned for *{reason}*",
+            f"{ban_emoji} {member.mention} has been banned for *{reason}*",
         )
 
     @discord.slash_command(
         name="unban",
         description="Unban a user from the server",
-        default_member_permissions=discord.Permissions(
-            ban_members=True
-        )
+        default_member_permissions=discord.Permissions(ban_members=True)
     )
     @commands.check(check_permissions_for_command)
     @commands.bot_has_permissions(ban_members=True)
@@ -81,26 +76,16 @@ class Moderation(commands.Cog):
     ):
         try:
             user_id = int(user.split("(")[-1].strip(")"))
-        except (ValueError, IndexError):
-            await ctx.response.send_message(
-                "An error occured when getting the user ID of the banned user, please try again!", ephemeral=True
-            )
-            return
-
-        discord_user = await self.bot.fetch_user(user_id)
-
-        try:
+            discord_user = await self.bot.fetch_user(user_id)
             await ctx.guild.unban(discord_user)
-        except discord.NotFound:
-            await ctx.response.send_message(
-                f"<@{user_id}> is not banned.", ephemeral=True
-            )
+        except (ValueError, IndexError):
+            await ctx.response.send_message(f"{failed_emoji} An error occured when getting the user ID of the banned user, please try again!", ephemeral=True)
             return
+        except discord.NotFound:
+            await ctx.response.send_message(f"{failed_emoji} <@{user_id}> is not banned.", ephemeral=True)
 
         await logger.log_unban(ctx, discord_user, notes)
-        await ctx.response.send_message(
-            f"<:unban_hammer:1411243154920964147> {discord_user.mention} has had their ban *revoked*.",
-        )
+        await ctx.response.send_message(f"{unban_emoji} {discord_user.mention} has had their ban *revoked*.")
 
 def setup(bot: discord.Bot):
     bot.add_cog(Moderation(bot))
